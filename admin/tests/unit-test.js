f956153e6697bcd9a2752037603e5571568e0a2f
juicebox013 <justin.nguyen335@gmail.com>
2025-05-11 18:54:37 -0700

    bug again

--- admin/tests/unit-test.js
+++ admin/tests/unit-test.js
@@ -1,5 +1,5 @@
 // Function Unit Tests
-import { subtract } from '../code-to-unit-tests/unit.test';
+import { subtract } from '../code-to-unit-tests/unit.test.js';
 
 test('subtracts 2 - 1 to equal 1', () => {
     expect(2 - 1).toBe(1);

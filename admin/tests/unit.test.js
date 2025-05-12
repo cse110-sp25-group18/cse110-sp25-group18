c1870453231e0df003075582d9e8b5336e043ee9
velyio <angelscortez@gmail.com>
2025-05-11 19:35:34 -0700

    test: fix typo

--- admin/tests/unit.test.js
+++ admin/tests/unit.test.js
@@ -2,7 +2,7 @@
 import { subtract, add } from '../code-to-unit-tests/unit.test';
 
 test('subtracts 2 - 1 to equal 1', () => {
-    expect(2 - 1).toBe(1);
+    expect(subtract(2 - 1)).toBe(1);
   });
 
 test('add 2 + 1 to equal 3', () => {

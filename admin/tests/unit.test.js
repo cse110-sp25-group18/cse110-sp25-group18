e0e8085d7b430b8e668c6610b8ef7256e00ff70b
velyio <angelscortez@gmail.com>
2025-05-11 19:43:19 -0700

    fix: yml rename file for unit tests

--- admin/tests/unit.test.js
+++ admin/tests/unit.test.js
@@ -3,7 +3,7 @@ import { subtract, add } from '../code-to-unit-tests/unit.test';
 
 test('subtracts 2 - 1 to equal 1', () => {
     expect(subtract(2 - 1)).toBe(1);
-  });
+});
 
 test('add 2 + 1 to equal 3', () => {
   expect(add(2,1)).toBe(3);

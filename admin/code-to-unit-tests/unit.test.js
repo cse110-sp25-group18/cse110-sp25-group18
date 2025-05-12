88f41bbed3fd535195d22bbe519814fefc50f0a2
thomas <trocha@ucsd.edu>
2025-05-11 19:43:31 -0700

    fix: import and exports for tests

--- admin/code-to-unit-tests/unit.test.js
+++
@@ -1,4 +0,0 @@
-export function subtract(a, b) {
-    return a - b;
-  }
-  
--- admin/tests/unit.test.js
+++ admin/tests/unit.test.js
@@ -1,10 +1,10 @@
 // Function Unit Tests
-import { subtract, add } from '../code-to-unit-tests/unit.test';
+import { sum } from '../code-to-unit-tests/unit.test';
 
 test('subtracts 2 - 1 to equal 1', () => {
     expect(2 - 1).toBe(1);
   });
 
 test('add 2 + 1 to equal 3', () => {
-  expect(add(2,1)).toBe(3);
+  expect(sum(2,1)).toBe(3);
 });
